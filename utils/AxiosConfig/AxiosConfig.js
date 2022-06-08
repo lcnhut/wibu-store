ce09537aa35abd11ccdf5967ec267a9547dc56ad
thangduongnfq <thang.duon@nfq.asia>
2022-06-08 13:37:57 +0700

    chore: remove utils folder outside src

--- utils/AxiosConfig/AxiosConfig.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const instance = axios.create({
-  baseURL: "https://629afeb4656cea05fc319faa.mockapi.io/",
-  timeout: 3000,
-  headers: { "X-Custom-Header": "foobar" },
-});
-
-export default instance;
