f7bff41448579a9a12c28df794032b7217511f4f
quoctrang.le <quoctrang.le@nfq.asia>
2022-06-09 12:06:59 +0700

    working on list product

--- src/pages/Product/ListProduct/SingleProduct/index.jsx
+++ src/pages/Product/ListProduct/SingleProduct/index.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import "./styles.scss";
+import { HeartOutlined, SearchOutlined } from "@ant-design/icons";
 
 const SingleProduct = ({ product }) => {
   return (
     <div className="single__product__wrapper">
       <div className="single__product__image">
-        <img src={product.image} alt="product image" />
+        <div className="single__product__image__contain">
+          <img src={product.image} alt="product image" />
+          <div className="single__product__image__boxmodal">
+            <span>
+              <HeartOutlined />
+            </span>
+            <span>...</span>
+            <span>
+              <SearchOutlined />
+            </span>
+          </div>
+        </div>
       </div>
       <div className="single__product__content">
         <h3>{product.name}</h3>
-        <h5>{product.price}</h5>
+        <h5>${product.price}</h5>
       </div>
       <div className="single__product__action">
         <button>clickmen</button>
--- src/pages/Product/ListProduct/index.jsx
+++ src/pages/Product/ListProduct/index.jsx
@@ -21,14 +21,35 @@ const ListProduct = () => {
     getAllProduct();
   }, []);
   return (
-    <Row gutter={[16, 26]} className="listproduct__wrapper">
-      {products &&
-        products.map((product, id) => (
-          <Col className="listproduct__item" key={id} span={6}>
-            <SingleProduct product={product} />
-          </Col>
-        ))}
-    </Row>
+    <>
+      <Row className="listproduct__title__wrapper">
+        <Col className="listproduct__title__container" span={24}>
+          <h1>Shop The Collection</h1>
+          <div className="listproduct__title__line"></div>
+          <div className="listproduct__title__action">
+            <button style={{ padding: "7px 0" }}>MEN</button>
+            <button style={{ padding: "7px 0" }}>WOMEN</button>
+          </div>
+        </Col>
+      </Row>
+      <Row gutter={[16, 26]} className="listproduct__wrapper">
+        {products &&
+          products.map((product, id) => (
+            <Col
+              xs={{ span: 24 }}
+              sm={{ span: 24 }}
+              md={{ span: 12 }}
+              lg={{ span: 8 }}
+              xl={{ span: 6 }}
+              className="listproduct__item"
+              key={id}
+              span={6}
+            >
+              <SingleProduct product={product} />
+            </Col>
+          ))}
+      </Row>
+    </>
   );
 };
 
