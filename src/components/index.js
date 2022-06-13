36c0aa3237a351ad8eb8a8c160022454249d37d0
thangduongnfq <thang.duon@nfq.asia>
2022-06-13 15:03:53 +0700

    add: update component CricleColor and ButtonSize

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import ButtonOfPage from "./Button/ButtonOfPage";
 import AddProductForm from "./Form/AddProductForm/AddProductForm";
 import Navbar from "./Navbar/Navbar";
-export { ButtonOfPage, AddProductForm, Navbar };
+import CricleColor from "./cricleColor/CricleColor";
+import ButtonSize from "./ButtonSize/ButtonSize";
+export { ButtonOfPage, AddProductForm, Navbar, CricleColor, ButtonSize };
