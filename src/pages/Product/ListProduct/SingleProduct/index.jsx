8e46a730cc773b5b374b5594cf68db4da529b841
lcnhut <nhut.le@nfq.asia>
2022-06-15 17:37:47 +0700

    reset field after adding an item to cart

--- src/pages/Product/ListProduct/SingleProduct/index.jsx
+++ src/pages/Product/ListProduct/SingleProduct/index.jsx
@@ -25,7 +25,7 @@ const SingleProduct = ({ product }) => {
   const [form] = Form.useForm();
   const dispatch = useDispatch();
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [visibleModalAddToCart, setVisibleModalAddToCart] = useState(false);
   const [singleProduct, setSingleProduct] = useState({});
 
   const handleAddToCart = (values) => {
@@ -36,7 +36,7 @@ const SingleProduct = ({ product }) => {
       description: product.description,
     };
     dispatch(addToCart(submitData));
-    setIsModalVisible(false);
+    setVisibleModalAddToCart(false);
   };
 
   const onFinish = () => {
@@ -52,7 +52,7 @@ const SingleProduct = ({ product }) => {
   };
 
   const handleCancel = () => {
-    setIsModalVisible(false);
+    setVisibleModalAddToCart(false);
   };
 
   const getProduct = async (id) => {
@@ -60,7 +60,7 @@ const SingleProduct = ({ product }) => {
     data && setSingleProduct(data);
   };
   const handleClick = (id) => {
-    setIsModalVisible(true);
+    setVisibleModalAddToCart(true);
     getProduct(id);
   };
 
@@ -83,7 +83,7 @@ const SingleProduct = ({ product }) => {
     form.setFieldsValue({
       size: sizeOption[0],
     });
-  }, [sizeOption]);
+  }, [sizeOption, visibleModalAddToCart]);
 
   // update size after changing color
   const onChangeColor = (selectedColor) => {
@@ -100,7 +100,7 @@ const SingleProduct = ({ product }) => {
       {singleProduct && (
         <Modal
           centered
-          visible={isModalVisible}
+          visible={visibleModalAddToCart}
           footer={null}
           onCancel={handleCancel}
           className="product__details__wrapper"
