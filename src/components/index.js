2ad477fee0b694fa7ef81b554a7ad0dcfd2a0c26
lcnhut <nhut.le@nfq.asia>
2022-06-16 14:02:29 +0700

    Change name folder

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,14 @@ import ButtonSize from './ButtonSize/ButtonSize';
 import CircleColor from './CircleColor/CircleColor';
 import Footer from './Footer/Footer';
 import AddProductForm from './Form/AddProductForm/AddProductForm';
+import UpdateProductForm from './Form/UpdateProductForm/UpdateProductForm';
 import Navbar from './Navbar/Navbar';
 import ProductDetailModal from './ProductDetailModal/ProductDetailModal';
 
 export {
   ButtonOfPage,
   AddProductForm,
+  UpdateProductForm,
   Navbar,
   CircleColor,
   ButtonSize,
