fee1a5416284040f1495ea311a3ed213b9401802
quoc-trang <quoc-trang@gmail.com>
2022-06-16 13:16:44 +0700

    update list product page

--- src/pages/Product/ListProduct/index.jsx
+++ src/pages/Product/ListProduct/index.jsx
@@ -1,39 +1,18 @@
 import { Col, Row } from "antd";
 import React, { useEffect, useState } from "react";
-import instance from "../../../utils/AxiosConfig/AxiosConfig";
 import SingleProduct from "./SingleProduct";
+import { getAllAsync } from "../../../store/productSlice";
 import "./styles.scss";
+import { useSelector, useDispatch } from "react-redux";
 
 const ListProduct = () => {
-  const [products, setProducts] = useState();
-  const getAllProduct = () => {
-    instance
-      .get("/products")
-      .then(function (response) {
-        const { data } = response;
-        console.log(data);
-        data && setProducts(data);
-      })
-      .catch(function (e) {
-        console.log(e);
-      });
-  };
-
+  const products = useSelector((state) => state.product.list);
+  const dispatch = useDispatch();
   useEffect(() => {
-    getAllProduct();
+    dispatch(getAllAsync());
   }, []);
   return (
     <>
-      <Row className="listproduct__title__wrapper">
-        <Col className="listproduct__title__container" span={24}>
-          <h1>Shop The Collection</h1>
-          <div className="listproduct__title__line"></div>
-          <div className="listproduct__title__action">
-            <button style={{ padding: "7px 0" }}>MEN</button>
-            <button style={{ padding: "7px 0" }}>WOMEN</button>
-          </div>
-        </Col>
-      </Row>
       <Row gutter={[16, 26]} className="listproduct__wrapper">
         {products &&
           products.map(
