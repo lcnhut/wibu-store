0ac98a4bb59e5e89f9f7a41c524e2b2e2b8aba56
quoc-trang <quoc-trang@gmail.com>
2022-06-17 03:16:12 +0700

    working on details page

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,10 +1,11 @@
-import { Carousel, Col, Image, Row } from 'antd';
+import { Carousel, Col, Image, Row, Space, Typography } from 'antd';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import { getAllAsync, getByIdAsync } from '../../store/product/productSlice';
 
+const { Title } = Typography;
 export default function Details() {
   const dispatch = useDispatch();
   const product = useSelector((state) => state.product.singleProduct);
@@ -12,15 +13,24 @@ export default function Details() {
   useEffect(() => {
     dispatch(getByIdAsync(id));
   }, []);
-  // console.log(product);
+  const { image } = product;
+  console.log(image);
   return (
     <div className="container-page" style={{ padding: '0 7vw' }}>
+      <Space className="details__page__title">
+        <Title level={5}>Home</Title>
+        <span> &gt; {product.name}</span>
+      </Space>
       <Row gutter={[26, 0]}>
         <Col xl={{ span: 8 }} lg={{ span: 8 }} md={{ span: 12 }}>
           <Carousel dotPosition="left" autoplay autoplaySpeed={500}>
-            {product.image.map((img, key) => {
-              <h1>{img.src}</h1>;
-            })}
+            {image ? (
+              image.map((img, key) => (
+                <Image key={key} src={img.src} width={100} />
+              ))
+            ) : (
+              <h1>loading</h1>
+            )}
           </Carousel>
         </Col>
         <Col xl={{ span: 8 }} lg={{ span: 8 }} md={{ span: 12 }}>
