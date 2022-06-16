7b9f635bde0b0e5e6813ed684486293242e4486e
lcnhut <nhut.le@nfq.asia>
2022-06-16 14:22:34 +0700

    refactor structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,16 @@
 import ButtonOfPage from './Button/ButtonOfPage';
 import ButtonSize from './ButtonSize/ButtonSize';
+import Carousel from './Carousel';
 import CircleColor from './CircleColor/CircleColor';
+import FilterField from './FilterField/FilterField';
 import Footer from './Footer/Footer';
 import AddProductForm from './Form/AddProductForm/AddProductForm';
 import UpdateProductForm from './Form/UpdateProductForm/UpdateProductForm';
+import LazyLoaderProduct from './Lazyloader/LazyLoaderProduct';
+import ListProduct from './ListProduct';
 import Navbar from './Navbar/Navbar';
 import ProductDetailModal from './ProductDetailModal/ProductDetailModal';
+import SingleProduct from './SingleProduct';
 
 export {
   ButtonOfPage,
@@ -16,4 +21,9 @@ export {
   ButtonSize,
   Footer,
   ProductDetailModal,
+  LazyLoaderProduct,
+  ListProduct,
+  Carousel,
+  FilterField,
+  SingleProduct,
 };
