822023e9a9b16a69974122ec5e1139acac86e424
lcnhut <nhut.le@nfq.asia>
2022-06-20 16:40:23 +0700

    add search feature

--- src/pages/Invoice/Invoice.jsx
+++ src/pages/Invoice/Invoice.jsx
@@ -1,7 +1,7 @@
 import { CheckCircleTwoTone, InfoCircleTwoTone } from '@ant-design/icons';
-import { Spin, Table } from 'antd';
+import { Input, Spin, Table } from 'antd';
 import moment from 'moment';
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -11,6 +11,8 @@ const Invoice = () => {
   const dispatch = useDispatch();
   const invoiceData = useSelector((state) => state.invoice.list);
   const isLoading = useSelector((state) => state.invoice.isLoading);
+  const [invoice, setInvoice] = useState(invoiceData);
+  const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
     dispatch(getAllInvoiceAsync());
@@ -60,18 +62,32 @@ const Invoice = () => {
     },
   ];
 
+  // Handle search product by name
+  const onSearch = (value) => {
+    const currValue = value;
+    setSearchValue(value);
+    const filteredData = invoiceData.filter(
+      (entry) =>
+        entry.customerName.toLowerCase().includes(currValue) ||
+        entry.customerName.toUpperCase().includes(currValue) ||
+        entry.price.toString().includes(currValue) ||
+        entry.contact.includes(currValue)
+    );
+    setInvoice(filteredData);
+  };
+
   return (
     <Spin tip="Loading..." size="large" spinning={isLoading}>
       <div className="admin__page">
-        {/* <Input
+        <Input
           className="search__input"
           placeholder="Search product"
           value={searchValue}
           onChange={(e) => onSearch(e.target.value)}
-        /> */}
+        />
         <Table
           className="product__table"
-          dataSource={invoiceData}
+          dataSource={invoice}
           columns={columns}
         />
       </div>
