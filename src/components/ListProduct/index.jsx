e3a26f671a7ab546068eb8f7be0644c291b2d475
thangduongnfq <thang.duon@nfq.asia>
2022-06-23 03:22:45 +0700

    add:  notification when filter not found item

--- src/components/ListProduct/index.jsx
+++ src/components/ListProduct/index.jsx
@@ -50,8 +50,7 @@ const ListProduct = (props) => {
                 </Col>
               ))}
           </>
-        ) : (
-          sliceData &&
+        ) : sliceData.length > 0 ? (
           sliceData.map((product, id) => {
             return (
               id < ROW_SHOW_IN_PAGE && (
@@ -70,6 +69,8 @@ const ListProduct = (props) => {
               )
             );
           })
+        ) : (
+          <p>Sorry, there are no products in this collection</p>
         )}
         <Col
           xs={{ span: 24 }}
@@ -78,7 +79,7 @@ const ListProduct = (props) => {
           lg={{ span: 24 }}
           xl={{ span: 24 }}
         >
-          {data && (
+          {sliceData.length > 0 && (
             <Pagination
               showSizeChanger
               style={{
